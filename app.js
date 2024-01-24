06543a95943b97f382bc5185cb65dee701967f6e
Aws_killer <telegramsuper6@gmail.com>
2024-01-24 14:20:20 +0000

    clean up time

--- app.js
+++ app.js
@@ -1,79 +1,216 @@
 const express = require('express');
 const cors = require('cors');
-const opentype = require('./opentype.js');
+const opentype = require('opentype.js');
+const makerjs = require('makerjs');
+const axios = require('axios');
 const fs = require('fs');
+const os = require('os');
 const path = require('path');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 
-function getTallestCharacter (text) {
-    let tallestCharacter = '';
-    if (text.includes('f')) {
-      tallestCharacter = 'f';
-    } else if (text.includes('b') || text.includes('d') || text.includes('h') || text.includes('k') || text.includes('l')) {
-      tallestCharacter = 'b';
-    } else if (text.includes('i') || text.includes('j')) {
-      tallestCharacter = 'i';
-    } else if (text.includes('t')) {
-      tallestCharacter = 't';
-    } else if (text.includes('o') || text.includes('p') || text.includes('q') || text.includes('r') || text.includes('s')) {
-      tallestCharacter = 's';
-    } else {
-      tallestCharacter = 'a';
+
+function handleRequest(err, loadedFont, config) {
+    const [text, fontName, size, union, filled, kerning, separate, bezierAccuracy, units, fill, stroke, strokeWidth, strokeNonScaling, fillRule, individualLetters, res] = config;
+
+    if (err) {
+        console.error('Font could not be loaded:', err);
+        return res.status(500).json({error: err.message});
     }
-    return tallestCharacter;
+
+    if (individualLetters) { // Generate individual SVGs for each letter and store them in a JSON object
+        const individualSVGs = {};
+        for (let i = 0; i < text.length; i++) {
+            const letter = text[i];
+            const result = callMakerjs(loadedFont, letter, size, union, filled, kerning, separate, bezierAccuracy, units, fill, stroke, strokeWidth, strokeNonScaling, fillRule);
+            individualSVGs[letter] = result.svg;
+        }
+        res.status(200).json(individualSVGs); // Return JSON object
+    } else { // Generate a single SVG for the entire text
+        const result = callMakerjs(loadedFont, text, size, union, filled, kerning, separate, bezierAccuracy, units, fill, stroke, strokeWidth, strokeNonScaling, fillRule);
+        res.status(200).send(result.svg);
+    }
 }
 
-app.post('/generateSVGPath', async (req, res) => {
-    const { text, size, fill } = req.body;
-    const fontSize = size || 72;
-    const fillColor = fill || 'black';
-    const fontPath = path.join(__dirname, 'fonts', 'LTMuseum-Reg.ttf');
-  
+
+async function downloadAndSaveFont(fontUrl) {
+    const tempDir = path.join(os.tmpdir(), 'downloaded-fonts');
     try {
-      const font = await opentype.load(fontPath);
+        const response = await axios.get(fontUrl, {
+            responseType: 'arraybuffer',
+            headers: {
+                'Accept-Encoding': 'identity'
+            }
+        });
+        let filename;
 
-      const tallestCharacter = getTallestCharacter(text);
-      const tallestCharacterPath = font.getPath(tallestCharacter, 0, 0, fontSize);
-      const tallestCharacterBbox = tallestCharacterPath.getBoundingBox();
-      const tallestCharacterHeight = (tallestCharacterBbox.y2 - tallestCharacterBbox.y1).toFixed(2);
+        // Try to extract filename from Content-Disposition header
+        const contentDisposition = response.headers['content-disposition'];
+        if (contentDisposition) {
+            const matches = /filename=(['"]?)(.+)\1/.exec(contentDisposition);
+            if (matches.length === 3) {
+                filename = matches[2];
+            }
+        }
 
-      var controlCharacterHeight = tallestCharacterHeight;
-      if (tallestCharacter != 'a') {
-        const controlCharacter = 'a';
-        const controlCharacterPath = font.getPath(controlCharacter, 0, 0, fontSize);
-        const controlCharacterBbox = controlCharacterPath.getBoundingBox();
-        controlCharacterHeight = (controlCharacterBbox.y2 - controlCharacterBbox.y1).toFixed(2);
-      }
+        // If filename not found in header, extract from URL
+        if (! filename) {
+            const urlParts = fontUrl.split('/');
+            filename = urlParts[urlParts.length - 1];
+        }
 
-      const path = font.getPath(text, 0, 0, fontSize);
-      const svgPath = path.toSVG();
-      const bbox = path.getBoundingBox();
-  
-      const width = (bbox.x2 - bbox.x1).toFixed(2);
-      const height = (bbox.y2 - bbox.y1).toFixed(2);
-      const viewBox = `${bbox.x1.toFixed(2)} ${bbox.y1.toFixed(2)} ${width} ${height}`;
-  
-      const svg = `
-        <svg width="${width}" height="${height}" viewBox="${viewBox}" xmlns="http://www.w3.org/2000/svg">
-          <g id="svgGroup" stroke-linecap="round" fill-rule="evenodd" font-size="${fontSize}" stroke="none" fill="${fillColor}">
-            ${svgPath}
-          </g>
-        </svg>
-      `;
-  
-      res.status(200).send({ tallestCharacter, tallestCharacterHeight, controlCharacterHeight, svg });
+        // Save the font file
+        const fontPath = path.join(tempDir, filename);
+        fs.writeFileSync(fontPath, response.data);
+        return fontPath;
     } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: error.message });
+        console.error('Error downloading the font:', error);
+        throw error;
     }
-  });
-  
+}
+
+
+function callMakerjs(font, text, size, union, filled, kerning, separate, bezierAccuracy, units, fill, stroke, strokeWidth, strokeNonScaling, fillRule) { // Generate the text using a font
+    var textModel = new makerjs.models.Text(font, text, size, union, false, bezierAccuracy, {kerning});
+
+    if (separate) {
+        for (var i in textModel.models) {
+            textModel.models[i].layer = i;
+        }
+    }
+
+    var svg = makerjs.exporter.toSVG(textModel, {
+        fill: filled ? fill : undefined,
+        stroke: stroke ? stroke : undefined,
+        strokeWidth: strokeWidth ? strokeWidth : undefined,
+        fillRule: fillRule ? fillRule : undefined,
+        scalingStroke: ! strokeNonScaling
+    });
+
+    var dxf = makerjs.exporter.toDXF(textModel, {
+        units: units,
+        usePOLYLINE: true
+    });
+
+    return {svg, dxf};
+}
+
+app.post('/generateSVGPath', async (req, res) => { // Set default values
+    const {
+        text,
+        size = 72, // Default font size
+        union = false,
+        filled = true,
+        kerning = true,
+        separate = false,
+        bezierAccuracy = 2,
+        units = 'mm', // Default units
+        fill = 'black', // Default fill color
+        stroke = 'none', // Default stroke color
+        strokeWidth = '1', // Default stroke width
+        strokeNonScaling = false,
+        fillRule = 'nonzero', // Default fill rule
+        fontUrl,
+        individualLetters = false,
+        font = 'Roobert-Regular.ttf' // Default local font
+    } = req.body;
+
+
+    const config = [
+        text,
+        font,
+        size,
+        union,
+        filled,
+        kerning,
+        separate,
+        bezierAccuracy,
+        units,
+        fill,
+        stroke,
+        strokeWidth,
+        strokeNonScaling,
+        fillRule,
+        individualLetters,
+        res
+    ]
+
+    const fontPath = fontUrl ? await downloadAndSaveFont(fontUrl) : path.join(__dirname, 'fonts', font);
+
+    opentype.load(fontPath, (err, loadedFont) => {
+        handleRequest(err, loadedFont, config);
+    });
+});
+
+
+app.post('/generateSVGPathWithGoogleFont', async (req, res) => { // Default values for parameters
+    const {
+        text,
+        fontName = 'Open Sans', // Default Google Font
+        size = 72, // Default font size
+        union = false, // Default union
+        filled = true, // Default filled
+        kerning = true, // Default kerning
+        separate = false, // Default separate
+        bezierAccuracy = 2, // Default bezierAccuracy
+        units = 'mm', // Default units
+        fill = 'black', // Default fill color
+        stroke = 'none', // Default stroke
+        strokeWidth = '1', // Default stroke width
+        strokeNonScaling = false, // Default strokeNonScaling
+        fillRule = 'nonzero', // Default fillRule
+        individualLetters = false
+    } = req.body;
+    const config = [
+        text,
+        fontName,
+        size,
+        union,
+        filled,
+        kerning,
+        separate,
+        bezierAccuracy,
+        units,
+        fill,
+        stroke,
+        strokeWidth,
+        strokeNonScaling,
+        fillRule,
+        individualLetters,
+        res
+    ]
+    const apiKey = 'AIzaSyAOES8EmKhuJEnsn9kS1XKBpxxp-TgN8Jc'; // Use environment variable for API key
+
+    try { // Fetch the list of fonts from Google Fonts API
+        const response = await axios.get(`https://www.googleapis.com/webfonts/v1/webfonts?key=${apiKey}`);
+        const fonts = response.data.items;
+
+        // Find the font with the given name
+        const fontDetails = fonts.find(f => f.family === fontName);
+        if (! fontDetails) {
+            return res.status(404).send('Font not found');
+        }
+
+
+        // Load the font using opentype.js
+        let fontUrl = fontDetails.files.regular; // Adjust based on font variants if needed
+        fontUrl = fontUrl.replace("http", 'https')
+        const fontPath = await downloadAndSaveFont(fontUrl);
+
+        opentype.load(fontPath, (err, loadedFont) => {
+            handleRequest(err, loadedFont, config);
+        });
+
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({error: error.message});
+    }
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
--- client.js
+++
@@ -1,5 +0,0 @@
-var app = new App();
-window.onload = function () {
-    app.init();
-    app.getGoogleFonts('AIzaSyAOES8EmKhuJEnsn9kS1XKBpxxp-TgN8Jc');
-};
